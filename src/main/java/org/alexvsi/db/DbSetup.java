69333e780b2957506ffbfdd7a886de73fed90886
Drvls <mestreunder@gmail.com>
2025-07-12 12:54:02 -0300

    UPDATE

--- src/main/java/org/alexvsi/db/DbSetup.java
+++ src/main/java/org/alexvsi/db/DbSetup.java
@@ -3,7 +3,6 @@ package org.alexvsi.db;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DbSetup {
     public static void criarDb(){
